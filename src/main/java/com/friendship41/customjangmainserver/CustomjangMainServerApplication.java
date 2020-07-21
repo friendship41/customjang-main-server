4c8c3417f8d2112c0edd215294229500354d7af5
friendship41 <stage41@nate.com>
2020-07-21 23:43:49 +0900

    Initial commit

---
+++ src/main/java/com/friendship41/customjangmainserver/CustomjangMainServerApplication.java
@@ -0,0 +1,13 @@
+package com.friendship41.customjangmainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomjangMainServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomjangMainServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/friendship41/customjangmainserver/CustomjangMainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendship41.customjangmainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomjangMainServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
